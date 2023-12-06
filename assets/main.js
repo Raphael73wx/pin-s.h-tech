865dfba4070b74054c9eb6d8fd5e68ee37a80e5f
Raphael73wx <raphael29wx@gmail.com>
2023-12-05 22:21:31 -0300

    atualização de carrossel para produtos.

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,24 @@
-document.getElementById("backbtn").addEventListener("click", 
-function () {
-    window.history.back;
-});
+
+//carrossel de produtos :
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+//botão voltar para pagina anterior:
+// document.getElementById("backbtn").addEventListener("click", 
+// function () {
+//     window.history.back;
+// });
